Validus-Shamu/vendor_validus

Attempt to get dtc into settings

config/common.mk
@@ -185,6 +185,9 @@ vendor/validus/prebuilt/common/app/adaway.apk:system/app/adaway.apk
 # Themes
 include vendor/validus/config/themes_common.mk
 
+#DragonTC
+-include vendor/validus/config/dtc.mk
+
 # Versioning System
 PRODUCT_VERSION_MAJOR = 6.0.1
 PRODUCT_VERSION_MINOR = alpha

tioguda/vendor_tesla

config/common.mk
@@ -176,6 +176,9 @@ PRODUCT_COPY_FILES += \
 endif
 endif
 
+#DragonTC
+-include vendor/tesla/config/dtc.mk
+
 # Versioning System
 # Tesla first version.
 PRODUCT_VERSION_MAJOR = 6.0.1